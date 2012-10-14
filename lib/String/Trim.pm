5878931d20cdd12ad98dcd1f7bccab01b4cddba7
Abhishek Shende <abhshekisnot@gmail.com>
2012-10-14 23:49:33 +0000

    Import of ABHIISNOT/String-Trim-0.001 from CPAN.
    gitpan-cpan-distribution: String-Trim
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         ABHIISNOT/String-Trim-0.001.tar.gz
    gitpan-cpan-author:       ABHIISNOT
    gitpan-cpan-maturity:     released

--- lib/String/Trim.pm
+++ lib/String/Trim.pm
@@ -1,184 +1,75 @@
-package String::Trim;
-# ABSTRACT: trim whitespace from your strings
 use strict;
 use warnings;
-our $VERSION = '0.005'; # VERSION
 
+package String::Trim;
+BEGIN {
+  $String::Trim::AUTHORITY = 'cpan:ABHIISNOT';
+}
+{
+  $String::Trim::VERSION = '0.001';
+}
 
-use Exporter 5.57 qw(import);
-our @EXPORT = qw(trim);
+# ABSTRACT: A module to experiment with dist zilla
+use Carp;
 
-
-sub trim { # Starting point: http://www.perlmonks.org/?node_id=36684
-    my $t = qr{^\s+|\s+$};
-    if (defined wantarray) {
-        @_ = (@_ ? @_ : $_);
-        if (ref $_[0] eq 'ARRAY') {
-            @_ = @{ $_[0] };
-            for (@_) { s{$t}{}g if defined $_ }
-            return \@_;
-        }
-        elsif (ref $_[0] eq 'HASH') {
-            foreach my $k (keys %{ $_[0] }) {
-                (my $nk = $k) =~ s{$t}{}g;
-                if (defined $_[0]->{$k}) {
-                    ($_[0]->{$nk} = $_[0]->{$k}) =~ s{$t}{}g;
-                }
-                else {
-                    $_[0]->{$nk} = undef;
-                }
-                delete $_[0]->{$k} unless $k eq $nk;
-            }
-        }
-        else {
-            for (@_ ? @_ : $_) { s{$t}{}g if defined $_ }
-        }
-        return wantarray ? @_ : $_[0];
-    }
-    else {
-        if (ref $_[0] eq 'ARRAY') {
-            for (@{ $_[0] }) { s{$t}{}g if defined $_ }
-        }
-        elsif (ref $_[0] eq 'HASH') {
-            foreach my $k (keys %{ $_[0] }) {
-                (my $nk = $k) =~ s{$t}{}g;
-                if (defined $_[0]->{$k}) {
-                    ($_[0]->{$nk} = $_[0]->{$k}) =~ s{$t}{}g;
-                }
-                else {
-                    $_[0]->{$nk} = undef;
-                }
-                delete $_[0]->{$k} unless $k eq $nk
-            }
-        }
-        else {
-            for (@_ ? @_ : $_) { s{$t}{}g if defined $_ }
-        }
-    }
+sub new {
+    my $class = shift;
+    my $self = { string => shift, };
+    return bless $self, $class;
 }
 
 
+sub string {
+    my $self = shift;
+    my $string = shift || $self->{string};
+    return $string;
+}
+
+
+sub trim {
+    my $self = shift;
+    $self->{string} || carp 'No String Specified';
+    $self->{string} =~ s/^\s+|\s+$//g;
+    return $self->{string};
+}
+
 1;
 
-
-
+__END__
 =pod
-
-=encoding utf-8
 
 =head1 NAME
 
-String::Trim - trim whitespace from your strings
+String::Trim - A module to experiment with dist zilla
 
 =head1 VERSION
 
-version 0.005
+version 0.001
 
-=head1 SYNOPSIS
+=head1 METHODS
 
-    use String::Trim;
+=head2 new
 
-    print "Do it? ";
-    trim(my $response = <>);
-    print "'$response'\n";
+Simplistic constructor for this class.
 
-=head1 DESCRIPTION
+=head2 string
 
-C<String::Trim> trims whitespace off your strings. L<chomp> trims only C<$/> (typically,
-that's newline), but C<trim> will trim all leading and trailing whitespace.
-
-=head1 FUNCTIONS
+A getter/setter method for our only variable.
 
 =head2 trim
 
-Returns the string with all leading and trailing whitespace removed. Trimming
-undef gives you undef. Alternatively, you can trim in-place.
+A method to trim a string.
 
-    my $var     = ' my string  ';
-    my $trimmed = trim($var);
-    # OR
-    trim($var);
+=head1 AUTHOR
 
-C<trim> also knows how to trim an array or arrayref:
-
-    my @to_trim = (' one ', ' two ', ' three ');
-    my @trimmed = trim(@to_trim);
-    # OR
-    trim(@to_trim);
-    # OR
-    my $trimmed = trim(\@to_trim);
-    # OR
-    trim(\@to_trim);
-
-=head1 RATIONALE
-
-C<trim> is often used by beginners, who may not understand how to spin their own. While
-L<String::Util> does have a C<trim> function, it depends on L<Debug::ShowStuff>, which
-depends on L<Taint>, which fails the test suite, and doesn't appear to be maintained.
-This module installs, is actively maintained, and has no non-core dependencies.
-
-Other options include L<Text::Trim> and L<String::Strip> (which is implemented in XS,
-and is therefore likely to be very fast, but requires a C compiler).
-
-=head1 AVAILABILITY
-
-The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you, or see L<http://search.cpan.org/dist/String-Trim/>.
-
-The development version lives at L<http://github.com/doherty/String-Trim>
-and may be cloned from L<git://github.com/doherty/String-Trim.git>.
-Instead of sending patches, please fork this project using the standard
-git and github infrastructure.
-
-=head1 SOURCE
-
-The development version is on github at L<http://github.com/doherty/String-Trim>
-and may be cloned from L<git://github.com/doherty/String-Trim.git>
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests through the web interface at
-L<https://github.com/doherty/String-Trim/issues>.
-
-=head1 CREDITS
-
-This module was inspired by code at L<http://www.perlmonks.org/?node_id=36684> written
-by japhy (Jeff Pinyan), dragonchild, and Aristotle. This Perl module was written by Mike
-Doherty.
-
-=head1 AUTHORS
-
-=over 4
-
-=item *
-
-Mike Doherty <doherty@cpan.org>
-
-=item *
-
-Jeff Pinyan <pinyan@cpan.org>
-
-=item *
-
-Rob Kinyon <rkinyon@cpan.org>
-
-=item *
-
-Αριστοτέλης Παγκαλτζής (Aristotle Pagaltzis) <aristotle@cpan.org>
-
-=back
+Abhishek Shende <abhishekisnot@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Mike Doherty.
+This software is copyright (c) 2012 by Abhishek Shende.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
 
-
-__END__
--- xt/release/cpan-changes.t
+++
@@ -1,7 +0,0 @@
-#!perl
-
-use Test::More;
-eval 'use Test::CPAN::Changes';
-plan skip_all => 'Test::CPAN::Changes required for this test' if $@;
-changes_file_ok('Changes');
-done_testing();
